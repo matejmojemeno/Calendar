2c13a2684473020a77cd2dbbc638ad064f1a3251
Matej <kulhama8@fit.cvut.cz>
2022-05-28 15:06:48 +0200

    fixed time

--- semestralka/src/time.cpp
+++ semestralka/src/time.cpp
@@ -2,25 +2,29 @@
 
 Time::Time()
 {
-    time_t rawTime;
-    struct tm *tmp;
-    tmp = std::localtime(&rawTime);
-    time = *tmp;
-    time.tm_sec = 0;
-    time.tm_isdst = 0;
+    std::time_t t = std::time(0);
+    struct tm *now = localtime(&t);
+
+    m_year = now->tm_year + 1900;
+    m_mon = now->tm_mon + 1;
+    m_day = now->tm_mday;
+    m_hour = m_min = 0;
 }
 
 Time::Time(int year, int mon, int day, int hour, int min)
 {
-    struct tm tmp = {0, min, hour, day, mon - 1, year - 1900, 0, 0, 0};
-    checkDate(tmp);
-    mktime(&tmp);
-    time = tmp;
+    m_year = year;
+    m_mon = mon;
+    m_day = day;
+    m_hour = hour;
+    m_min = min;
+
+    checkDate();
 }
 
 bool Time::operator<(const Time &rs) const
 {
-    return less(time, rs.time);
+    return std::tie(m_year, m_mon, m_day, m_hour, m_min) < std::tie(rs.m_year, rs.m_mon, rs.m_day, rs.m_hour, rs.m_min);
 }
 
 bool Time::operator==(const Time &rs) const
@@ -28,32 +32,22 @@ bool Time::operator==(const Time &rs) const
     return !(*this < rs || *this < rs);
 }
 
-bool Time::less(const struct tm &ls, const struct tm &rs) const
-{
-    return std::tie(ls.tm_year, ls.tm_mon, ls.tm_mday, ls.tm_hour, ls.tm_min) < std::tie(rs.tm_year, rs.tm_mon, rs.tm_mday, rs.tm_hour, rs.tm_min);
-}
-
-bool Time::equal(const struct tm &ls, const struct tm &rs) const
-{
-    return !(less(ls, rs) || less(ls, rs));
-}
-
 bool Time::isLeap(int year) const
 {
     return year % 4000 && (!(year % 400) || (year % 100 && !(year % 4)));
 }
 
-//int Time::monthDays(int year, int mon) const
-//{
-//    if (mon == 2)
-//        return 28 + isLeap(year);
-//    if (mon == 4 || mon == 6 || mon == 9 || mon == 11)
-//        return 30;
-//    return 31;
-//}
+int Time::monthDays() const
+{
+    if (m_mon == 2)
+        return 28 + isLeap(m_year);
+    if (m_mon == 4 || m_mon == 6 || m_mon == 9 || m_mon == 11)
+        return 30;
+    return 31;
+}
 
-int Time::monthDays() const {
-    int year = time.tm_year + 1900, mon = time.tm_mon + 1;
+int Time::monthDays(int year, int mon) const
+{
     if (mon == 2)
         return 28 + isLeap(year);
     if (mon == 4 || mon == 6 || mon == 9 || mon == 11)
@@ -61,19 +55,78 @@ int Time::monthDays() const {
     return 31;
 }
 
-bool Time::checkDate(struct tm time) const
+bool Time::checkDate() const
 {
-    struct tm copy = time;
-    time_t check = mktime(&time);
-    if (!(equal(time, copy)) || check == -1)
+    if (m_mon < 1 || m_day < 1 || m_hour < 0 || m_min < 0)
+        return false;
+    if (m_mon > 12 || m_day > monthDays() || m_hour > 23 || m_min > 59)
         return false;
     return true;
 }
 
 int Time::dayNumber() const
 {
-    int year = time.tm_year + 1900, month = time.tm_mon + 1, day = time.tm_mday;
     static int t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
-    year -= month < 3;
-    return (year + year / 4 - year / 100 + year / 400 + t[month - 1] + day) % 7;
+    int year = m_year - (m_mon < 3);
+    return (year + year / 4 - year / 100 + year / 400 - year / 4000 + t[m_mon - 1] + m_day) % 7;
+}
+
+int Time::dayNumber(int year, int mon, int day) const
+{
+    static int t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
+    year -= mon < 3;
+    return (year + year / 4 - year / 100 + year / 400 - year / 4000 + t[mon - 1] + day) % 7;
+}
+
+int Time::countLeapYears(int year) const
+{
+    return (year / 4 - year / 100 + year / 400 - year / 4000);
+}
+
+long long Time::toDays() const
+{
+    long long days = m_year * 365 + countLeapYears(m_year - 1);
+
+    for (int i = 1; i < m_mon; i++)
+        days += monthDays(m_year, i);
+
+    return days + m_day;
+}
+
+Time &Time::toDate(long long days)
+{
+    m_year = 0;
+    m_mon = m_day = 1;
+    while ((days - toDays()) / 366) {
+        m_year += (days - toDays()) / 366;
+    }
+    days -= toDays();
+
+    if (days == 365 && !isLeap(m_year)) {
+        m_year++;
+        days = 0;
+    }
+
+    while (days >= monthDays()) {
+        days -= monthDays();
+        m_mon++;
+    }
+
+    m_day += days;
+
+    return *this;
+}
+
+Time &Time::operator+(int amount) {
+    if (! amount)
+        return *this;
+    toDate(toDays() + amount);
+    return *this;
+}
+
+Time &Time::operator-(int amount) {
+    if (! amount)
+        return *this;
+    toDate(toDays() - amount);
+    return *this;
 }
--- semestralka/src/time.h
+++ semestralka/src/time.h
@@ -9,13 +9,18 @@ public:
     bool operator<(const Time &) const;
     bool operator==(const Time &) const;
     bool isLeap(int) const;
-    //int monthDays(int, int) const;
+    int monthDays(int, int) const;
     int monthDays() const;
-    bool checkDate(struct tm) const;
+    bool checkDate() const;
     int dayNumber() const;
+    int dayNumber(int, int, int) const;
+    int countLeapYears(int) const;
+    long long toDays() const;
+    Time &toDate(long long);
+    Time &operator+(const int);
+    Time &operator-(const int);
 
-    struct tm time;
+    int m_year, m_mon, m_day, m_hour, m_min;
+
 private:
-    bool less(const struct tm &, const struct tm &) const;
-    bool equal(const struct tm &, const struct tm &) const;
 };
