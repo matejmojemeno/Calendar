5124b865b89a40d007093723e6e1cd27d4fcbd5e
Matej <kulhama8@fit.cvut.cz>
2022-06-06 14:14:53 +0200

    the last one, hopefully

--- semestralka/src/event.cpp
+++ semestralka/src/event.cpp
@@ -1,5 +1,16 @@
 #include "event.h"
 
+Event::Event()
+{
+    std::vector<std::string> emptyVector;
+    start = Time();
+    end = Time();
+    name = "";
+    place = "";
+    participants = emptyVector;
+    rep = 0;
+}
+
 Event::Event(const Time &s, const Time &e, const std::string &n, const std::string &p, const std::vector<std::string> &part, int r)
 {
     start = s;
@@ -23,7 +34,7 @@ void Event::displaySmall(int line, bool highlighted) const
     attroff(HIGHLIGHT);
 }
 
-void Event::displayFull() const 
+int Event::displayFull() const 
 {
     clear();
     move(0, 0);
@@ -33,12 +44,60 @@ void Event::displayFull() const
     printw(place.c_str());
     mvprintw(1, 0, "Start: %02d.%02d.%d %02d:%02d", start.m_day, start.m_mon, start.m_year, start.m_hour, start.m_min);
     mvprintw(2, 0, "End: %02d.%02d.%d %02d:%02d", end.m_day, end.m_mon, end.m_year, end.m_hour, end.m_min);
+    move(3, 0);
+    for (auto person : participants) {
+        printw("%s, ", person.c_str());
+    }
+    move(4, 0);
+    printw("Repetition: %s", getRep(rep).c_str());
+    printw("\nPress 1 - export, 2 - remove, 3 - move");
 
-    getch();
+    int c = getch();
+    while (c != 4) {
+        refresh();
+        printw("\nPress 1 - export, 2 - remove, 3 - move");
+        if (c == '1')
+            return c;  
+        else if (c == '2')
+            return c; 
+        c = getch();
+    }
+
+    return 0;
 }
 
 
 Event Event::operator+ (int amount)
 {
     return Event(start + amount, end + amount, name, place, participants, rep);
+}
+
+void Event::exportEvent() const {
+    std::ofstream f("examples/" + name + ".txt");
+
+    if (! f.is_open() || ! f.good())
+        throw std::invalid_argument("Cannot write into file.");
+    
+    f << "start = " << start.m_day << "." << start.m_mon << "." << start.m_year << " " << start.m_hour << ":" << start.m_min << "\n";
+    f << "end = " << end.m_day << "." << end.m_mon << "." << end.m_year << " " << end.m_hour << ":" << end.m_min << "\n";
+    f << "name = " << name << "\n";
+    f << "place = " << place << "\n";
+    f << "participants = ";
+    for (auto person : participants)
+        f << person << ", ";
+    f << "\n" << getRep(rep);
+}
+
+std::string Event::getRep(int rep) const {
+    if (rep == 0)
+        return "never";
+    if (rep == 1)
+        return "every day";
+    if (rep == 2)
+        return "every week";
+    if (rep == 3)
+        return "every 14 days";
+    if (rep == 4)
+        return "every month";
+    return "never";
 }
--- semestralka/src/eventStorage.h
+++ semestralka/src/eventStorage.h
@@ -9,29 +9,125 @@
 class EventStorage
 {
 public:
+    /**
+     * @brief 
+     * adds new event
+     */
     void addEvent(const Event &);
+    /**
+     * @brief 
+     * returns all events happening on given day
+     * @return std::vector<Event> 
+     */
     std::vector<Event> dayEvents(const Time &) const;
+    /**
+     * @brief 
+     * finds event with given name
+     */
     void findName(const std::string &, bool &, Event &) const;
-
+    /**
+     * @brief 
+     * removes event with given name if exists
+     */
+    void removeEvent(const std::string &);
+    /**
+     * @brief 
+     * moves event to a new date
+     */
+    void moveEvent(const Event &, const Time &);
+    /**
+     * @brief 
+     * vectors to store all events
+     */
     std::vector<Event> allEvents;
     std::vector<Event> eventsByStart;
+    std::vector<Event> eventsByEnd;
     std::vector<Event> dailyEvents;
     std::vector<Event> weeklyEvents;
-    std::vector<Event> twoWeeklyEvent;
+    std::vector<Event> twoWeeklyEvents;
     std::vector<Event> monthlyEvents;
 
 private:
+    /**
+     * @brief 
+     * check if event can be added
+     * @return true 
+     * @return false 
+     */
     bool checkEvent(const Event &) const;
+    /**
+     * @brief 
+     * check if date of event doesnt collide with other events
+     * @return true 
+     * @return false 
+     */
     bool checkDate(const Event &) const;
+    /**
+     * @brief 
+     * check if name of event doesnt collide with other events
+     * @return true 
+     * @return false 
+     */
     bool checkName(const std::string &) const;
+    /**
+     * @brief 
+     * finds new starting date to event
+     * @return Time 
+     */
+    Time findNewDate(const Event &) const;
+    /**
+     * @brief 
+     * removes event from vector without repetition
+     */
+    void removeNoRep(const std::string &);
+    /**
+     * @brief 
+     * removes event from repetition vectors
+     */
+    void removeRep(const std::string &, std::vector<Event> &);
+    /**
+     * @brief 
+     * requests a new name for event
+     */
     void newName(const Event &);
+    /**
+     * @brief 
+     * iterates all events to check dates
+     * @return true 
+     * @return false 
+     */
     bool iterateEvents(const Event &, const std::vector<Event> &) const;
-    std::vector<Event> genRepeated(const Event &) const;
-    bool emptyRepeated() const;
+    /**
+     * @brief 
+     * check if two events collide
+     * @return true 
+     * @return false 
+     */
     bool intersectEvent(const Event &, const Event &) const;
+    /**
+     * @brief 
+     * inserts event
+     */
     void insert(const Event &);
+    /**
+     * @brief 
+     * inserts event while sorting based on start of event
+     */
     void insertStart(const Event &);
+    /**
+     * @brief 
+     * inserts event while sorting based on end of event
+     */
+    void insertEnd(const Event &);
+    /**
+     * @brief 
+     * inserts repeated event
+     */
     void insertRepeated(const Event &);
+    /**
+     * @brief 
+     * functions called by dayEvents
+     */
     void sameDay(std::vector<Event> &, const Time &) const;
     void sameDayReps(std::vector<Event> &, const Time &) const;
     void sameWeekReps(std::vector<Event> &, const Time &) const;
